7b3641f106985b55e3cadb391ec9372860a6c07e
supriadi-yusuf <supriadi96@gmail.com>
2020-11-13 14:53:53 +0700

    add example of database transaction

---
+++ databasex/databasex_example_03_test.go
@@ -0,0 +1,138 @@
+package databasex_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/supriadi-yusuf/mulai/databasex"
+)
+
+func Example_insert03() {
+
+	type Student struct {
+		ID    string
+		Name  string
+		Age   int
+		Grade int
+	}
+
+	postgres, err := databasex.NewPostgre("scott", "tiger", "localhost", "5432", "db_belajar_golang",
+		"?sslmode=disable", 0, 0)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db, err := postgres.GetDbConnection()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+
+	tx, err := db.BeginTx(context.Background(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sqlOp := databasex.NewSimpleSQL(postgres)
+
+	// prepare data
+	student := Student{"C001", "junjun", 6, 1}
+
+	// create model
+	model := databasex.NewSimpleModel("tb_student", student)
+
+	// insert data into table
+	if err = sqlOp.InsertTrans(context.Background(), tx, model); err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	tx.Commit() // or tx.Rollback()
+}
+
+func Example_delete03() {
+
+	// connect to postgresql with database db_belajar_golang
+	postgres, err := databasex.NewPostgre("scott", "tiger", "localhost", "5432", "db_belajar_golang",
+		"?sslmode=disable", 0, 0)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// get database connection
+	db, err := postgres.GetDbConnection()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+
+	tx, err := db.BeginTx(context.Background(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// create sql operatio object
+	sqlOp := databasex.NewSimpleSQL(postgres)
+
+	// create model we want to work with
+	model := databasex.NewSimpleModel("tb_student", nil)
+
+	// delete record for student whose name is agus
+	if _, err = sqlOp.DeleteTrans(context.Background(), tx, model, "name='agus'"); err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	// delete all records
+	if _, err = sqlOp.DeleteTrans(context.Background(), tx, model, ""); err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	tx.Commit() // or 	tx.Rollback()
+
+}
+
+func Example_update03() {
+
+	postgres, err := databasex.NewPostgre("scott", "tiger", "localhost", "5432", "db_belajar_golang",
+		"?sslmode=disable", 0, 0)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db, err := postgres.GetDbConnection()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+
+	tx, err := db.BeginTx(context.Background(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sqlOp := databasex.NewSimpleSQL(postgres)
+
+	const (
+		cName  = "eko win"
+		cAge   = 8
+		cGrade = 3
+	)
+
+	var keypair = struct {
+		Name  string
+		Age   int
+		Grade int
+	}{cName, cAge, cGrade}
+
+	// create model
+	model := databasex.NewSimpleModel("tb_student", keypair)
+
+	if _, err = sqlOp.UpdateTrans(context.Background(), tx, model, "id='C001'"); err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	tx.Commit() // or 	tx.Rollback()
+
+}
