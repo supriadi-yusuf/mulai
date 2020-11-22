05c24463cbdb89b4f403b20b9e3313db4c450670
supriadi-yusuf <supriadi96@gmail.com>
2020-11-22 12:37:43 +0700

    modify description of IValue and ICollection

--- simhelper/simhelper.go
+++ simhelper/simhelper.go
@@ -1,34 +1,67 @@
-//Package simhelper is simple helper function to help developer working with data/value
+//Package simhelper is simple helper function to help developer working with data/value.
 package simhelper
 
-//IValue is interface defines what to do with single data / value
+// IValue is interface defines what to do with single data / value.
+//
+// This interface has several methods :
+//
+// - IsIn(collection interface{}) (result bool, err error)
+//   Check if certain value is in collection of data.
+//   Parameter collection must be array, slice or map
+//
+// - IsNumber() (result bool)
+//
+//   Check if certain value is number or not.
+//
+// - ToFloat() (result float64, err error)
+//
+//   Converts certain value into float64
+//
+//
+// IValue is declarated as follow :
+//
 type IValue interface {
-	//IsIn is method to check if certain value is in collection of data
-	//parameter collection must be array, slice or map
 	IsIn(collection interface{}) (result bool, err error)
 	IsNumber() (result bool)
 	ToFloat() (result float64, err error)
 }
 
-//ICollection is interface defines what to do with collection of data ( map, slice, array)
+// ICollection is interface defines what to do with collection of data ( map, slice, array, struct).
+//
+// This interface has several methods :
+//
+// - RemoveIndex(index interface{}) (result interface{}, err error)
+//   Remove element with certain index from collection (map, array, list).
+//
+// - FilterValue(fcriteria interface{}) (result interface{}, err error)
+//   Filter data in collection (map, array, list) based on input parameter named fcriteria.
+//   Parameter fcriteria must be function.
+//
+// - MappingValue(fmapping interface{}) (result interface{}, err error)
+//   Map every value in collection (map, array, list) based on input parameter named fmapping.
+//   Parameter fmapping must be function.
+//
+// - MeanValue() (result float64, err error)
+//   Count average of value in collection
+//
+// - IsEqual(data interface{}) (result bool, err error)
+//   Check if two map/slice/struct are equal or not.
+//   Parameter data must be map/slice/struct.
+//
+// - ConvElmToInterface() (result interface{}, err error)
+//   Converts each element in slice / map to interface{}
+//
+// ICollection is declarated as follow :
 type ICollection interface {
-
-	//Remove element with certain index from collection
 	RemoveIndex(index interface{}) (result interface{}, err error)
 
-	//Filter collection by criteria based on fcriteria. fcriteria is function
 	FilterValue(fcriteria interface{}) (result interface{}, err error)
 
-	//Mapping is map every value on collection based on function fmapping.
 	MappingValue(fmapping interface{}) (result interface{}, err error)
 
-	//Mean is count average of data in collection
 	MeanValue() (result float64, err error)
 
-	//IsEqual check if two map/slice are equal or not
-	//parameter data must be slice or map
 	IsEqual(data interface{}) (result bool, err error)
 
-	//Converts each element in slice / map to interface{}
 	ConvElmToInterface() (result interface{}, err error)
 }
