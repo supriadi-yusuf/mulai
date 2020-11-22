04b50b11e53b744b39221832e989269c27a8b5be
supriadi-yusuf <supriadi96@gmail.com>
2020-11-22 14:43:14 +0700

    modify description of NewCollection

--- simhelper/collection.go
+++ simhelper/collection.go
@@ -180,6 +180,8 @@ func (c *myCollection) ConvElmToInterface() (result interface{}, err error) {
 }
 
 //NewCollection is function creating object whose type is ICollection
+//
+//data must be array, slice, map or struct
 func NewCollection(data interface{}) ICollection {
 	collection := new(myCollection)
 	collection.data = data
